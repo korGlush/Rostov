e29cb1f37be47bf2eb6cf301fa205edb8126fc4d
Glush <90596091+korGlush@users.noreply.github.com>
2022-02-01 16:26:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,28 +1,83 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    const modal = document.querySelector('.modal');
-    const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-    const closeBtn = document.querySelector ('.modal__close')
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible');
-    }
-    modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);
-    });
-    
-    closeBtn.addEventListener('click', switchModal)
 
-    document.addEventListener('click', (e) => {
-        if (e.target.classList === modal.classList) {
-            modal.classList.toggle('modal--visible');
-        };
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle="modal"]'),
+        closeBtn = $('.modal__close');
+        modalDialog = $('.modal__dialog')
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal-visible')
     });
-    
-    document.addEventListener('keydown', (e) => {
-        if(e.key == 'Escape') {
-            if (modal.classList.contains('modal--visible')) {
-                modal.classList.toggle('modal--visible');
-            };
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal-visible')
+    });
+
+    $(document).mouseup( function(e) {
+        if(!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0 ) {
+            modal.removeClass('modal-visible')
         };
     });
 
-})
+    $(document).on('keydown', function(e) {
+    if (e.keyCode == 27)
+    modal.removeClass('modal-visible')
+    });
+
+
+    const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+     },
+    });
+
+
+    var next = $('.swiper-button-next')
+    var prev = $('.swiper-button-prev')
+    var bullets =$('.swiper-pagination')
+
+
+    next.css('left', prev.width() + 10 + bullets.width() + 10)
+    bullets.css('left', prev.width() + 10)
+
+    new WOW().init();
+
+    // Валидация формы
+
+    $('.modal__form').validate({
+        errorClass: "invalid",
+        rules: {
+            // simple rule, converted to {required:true}
+            userName: {
+                required: true,
+                minlength: 2,
+            },
+            userPhone: "required",
+            // compound rule
+            userEmail: {
+              required: true,
+              email: true
+            }
+          },
+        messages: {
+        userName:{
+            required: "Имя обязательно",
+            minlength: "Имя не короче двух букв"
+        },
+        userPhone: "Телефон обязателен",
+        userEmail: {
+            required: "Обязательно укажите email",
+            email: "Введите в формате: name@domain.com"
+        }
+        }
+    });
+    
+    // Маска для телефона
+
+    $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) ___-__-__"});
+});
